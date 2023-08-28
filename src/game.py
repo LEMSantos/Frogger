b3f2bccc532bce5ca6850d6ddd3032d1ce5a9cf2
LEMSantos <lu.casmou.ra177833@gmail.com>
2023-08-28 20:01:04 -0300

    Adiciona a tela inicial

---
+++ src/game.py
@@ -0,0 +1,27 @@
+import sys
+
+import pygame
+
+from settings import GAME_TITLE, WINDOW_WIDTH, WINDOW_HEIGHT
+
+
+class Game:
+    def __init__(self):
+        pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption(GAME_TITLE)
+
+        self.__display_surface = pygame.display.get_surface()
+
+    def __handle_events(self) -> None:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+    def run(self) -> None:
+        while True:
+            self.__handle_events()
+
+            self.__display_surface.fill("black")
+
+            pygame.display.update()
